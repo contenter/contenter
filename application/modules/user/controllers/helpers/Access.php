e93920c2027b3efeac09ef3b080638cb38f24782
goldie <yuryz@finalview.com>
2010-04-02 11:04:57 +0300

    user_module init commit
    Signed-off-by: goldie <yuryz@finalview.com>

---
+++ application/modules/user/controllers/helpers/Access.php
@@ -0,0 +1,41 @@
+<?php
+
+class User_Controller_Helper_Access 
+    extends FinalView_Controller_Action_Helper_Access
+{
+    protected $users;   
+    
+    public function preDispatch()
+    {
+        $this->users = $this->getActionController()->getHelper('User');
+        parent::preDispatch();
+    }    
+    
+    protected function _userIndexIndexAccess(array $params = array()) 
+    {
+        if (!$this->users->requested && !$this->users->logged) {
+            $this->_status_code = self::STATUS_FORBIDDEN;
+        }
+    }
+    
+    protected function _userRegisterRegisterAccess(array $params = array()) 
+    {
+        if ($this->users->logged) {
+            $this->_status_code = self::STATUS_FORBIDDEN;
+        }        
+    }
+    
+    protected function _userAuthLoginAccess(array $params = array()) 
+    {
+        if ($this->users->logged) {
+            $this->_status_code = self::STATUS_FORBIDDEN;
+        }        
+    }
+    
+    protected function _userAuthLogoutAccess(array $params = array()) 
+    {
+        if (!$this->users->logged) {
+            $this->_status_code = self::STATUS_FORBIDDEN;
+        }        
+    }    
+}
