a64c4f6817b5a8c279e65f8afc3f3e6feea2c046
goldie <yuryz@finalview.com>
2010-04-02 12:25:20 +0300

    admin_module init commit
    Signed-off-by: goldie <yuryz@finalview.com>

---
+++ application/modules/admin/controllers/helpers/Access.php
@@ -0,0 +1,34 @@
+<?php
+
+class Admin_Controller_Helper_Access 
+    extends FinalView_Controller_Action_Helper_Access
+{
+    protected $users;   
+    
+    public function preDispatch()
+    {
+        $this->users = $this->getActionController()->getHelper('User');
+        parent::preDispatch();
+    }    
+    
+    protected function _adminIndexIndexAccess(array $params = array()) 
+    {
+        if (!$this->users->isLogged(User_Roles::ADMIN)) {
+            $this->_status_code = self::STATUS_FORBIDDEN;
+        }
+    }
+    
+    protected function _adminAuthLoginAccess(array $params = array()) 
+    {
+        if ($this->users->isLogged(User_Roles::ADMIN) ) {
+            $this->_status_code = self::STATUS_FORBIDDEN;
+        }        
+    }
+    
+    protected function _adminAuthLogoutAccess(array $params = array()) 
+    {
+        if (!$this->users->isLogged(User_Roles::ADMIN)) {
+            $this->_status_code = self::STATUS_FORBIDDEN;
+        }        
+    }    
+}
