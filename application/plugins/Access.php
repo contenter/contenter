0afa6574515bffa3b96fd2eb5b84eeae637199d2
goldie <yuryz@finalview.com>
2010-04-19 17:17:29 +0300

    User access rules and resources
    Signed-off-by: goldie <yuryz@finalview.com>

--- application/plugins/Access.php
+++ application/plugins/Access.php
@@ -2,5 +2,32 @@
 
 class Application_Plugin_Access extends FinalView_Controller_Plugin_Access
 {
-
+    protected function _notDetectedUserHandler()
+    {
+        $failedRule = $this->getResource()->getAccessRule();
+        switch (true) {
+            case $failedRule->isFailedRule('user_exist'): 
+                $this->_denyHandler();
+            break;
+            case $failedRule->isFailedRule('logged_in'):
+                $this->_redirectToLogin();
+            break;
+        }
+    }
+    
+    protected function _defaultHandler()
+    {
+        switch (true) {
+            case is_null($this->getResource()):
+                return;
+            break;
+        }
+        
+        parent::_defaultHandler();
+    }
+    
+    protected function _redirectToLogin()
+    {
+        Zend_Controller_Action_HelperBroker::getStaticHelper('Redirector')->gotoRoute(array(), 'UserAuthLogin');
+    }
 }
