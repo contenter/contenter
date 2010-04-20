cc8365048e8cf207610c5dcfc97b3bb2deac73be
goldie <yuryz@finalview.com>
2010-04-20 12:02:06 +0300

    temp commit
    Signed-off-by: goldie <yuryz@finalview.com>

--- application/plugins/Access.php
+++ application/plugins/Access.php
@@ -2,5 +2,13 @@
 
 class Application_Plugin_Access extends FinalView_Controller_Plugin_Access
 {
-
+    protected function _notDetectedAdminHandler()
+    {
+        $this->_redirectToAdminLogin();
+    }
+    
+    protected function _redirectToAdminLogin()
+    {
+        Zend_Controller_Action_HelperBroker::getStaticHelper('Redirector')->gotoRoute(array(), 'AdminAuthLogin' );
+    }
 }
