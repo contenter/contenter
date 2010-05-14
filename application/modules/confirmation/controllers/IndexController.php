fd5f556072a5f7d8d209b475cffc13b70a837ee1
goldie <yuryz@finalview.com>
2010-05-14 16:19:54 +0300

    improved confirmation update
    Signed-off-by: goldie <yuryz@finalview.com>

--- application/modules/confirmation/controllers/IndexController.php
+++ application/modules/confirmation/controllers/IndexController.php
@@ -6,29 +6,24 @@
 */
 class Confirmation_IndexController extends FinalView_Controller_Action 
 {
-    
-    const ACTION_ACCEPT = 'ACTION_ACCEPT';
-    const ACTION_DECLINE = 'ACTION_DECLINE';
-    
-    protected $_confirmation;   
-        
+            
     public function acceptAction()
     {
-        $confirmation = $this->_helper->confirmation;
+        $confirmation = $this->_helper->confirmation();
 
         $this->_forward($confirmation->confirmation_type, $confirmation->entity_model, 'confirmation', array(
             'hash'  =>  $this->getRequest()->getParam('hash'),
-            'reply' =>  self::ACTION_ACCEPT
+            'reply' =>  Confirmation::ACTION_ACCEPT
         ));
     }
     
     public function declineAction()
     {
-        $confirmation = $this->_helper->confirmation;
+        $confirmation = $this->_helper->confirmation();
         
         $this->_forward($confirmation->confirmation_type, $confirmation->entity_model, 'confirmation', array(
             'hash'  =>  $this->getRequest()->getParam('hash'),
-            'reply' =>  self::ACTION_DECLINE
+            'reply' =>  Confirmation::ACTION_DECLINE
         ));
     }    
 }
