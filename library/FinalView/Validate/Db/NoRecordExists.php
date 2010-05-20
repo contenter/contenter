5c7a1ccae39c07dc148c486ec27b75e62501b55f
goldie <yuryz@finalview.com>
2010-05-20 13:43:04 +0300

    DB validators changed and FV_Doctrine_Table countbyparams method added
    Signed-off-by: goldie <yuryz@finalview.com>

--- library/FinalView/Validate/Db/NoRecordExists.php
+++ library/FinalView/Validate/Db/NoRecordExists.php
@@ -9,16 +9,17 @@ class FinalView_Validate_Db_NoRecordExists extends FinalView_Validate_Db_Abstrac
     
     public function isValid($value)
     {
-        $valid = true;
-        $this->_setValue($value);
-
-        $result = $this->_query($value);
-        if ($result) {
-            $valid = false;
-            $this->_error(self::ERROR_RECORD_FOUND);
-        }
-
-        return $valid;
+        $valid = true; 
+        $this->_setValue($value); 
+        
+        $count = $this->_getRecordsCount($value);  
+        
+        if ($count > 0) { 
+            $valid = false; 
+            $this->_error(self::ERROR_RECORD_FOUND); 
+        } 
+         
+        return $valid; 
     }
     
 }
