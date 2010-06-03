d3e8e8f1e6d6d6c734198c5c4daa2fae18c04aba
Andrey Malinovski <andrey.malinovski@finalview.com>
2010-06-03 12:08:51 +0300

    [*] [User forms login] add decorator 'FvformErrors'

--- application/modules/user/forms/Login.php
+++ application/modules/user/forms/Login.php
@@ -10,6 +10,9 @@ class User_Form_Login extends Zend_Form
      */
     public function init()
     {
+        parent::init();
+        
+        // Email
         $element = new Zend_Form_Element_Text('email');
         $element
             ->setLabel('Email')
@@ -19,28 +22,34 @@ class User_Form_Login extends Zend_Form
             ;
         $this->addElement($element);
         
+        // Password
+        $min_password_length = null; 
+        $max_password_length = null;
         extract(FinalView_Config::get('user', 
             array('min_password_length', 'max_password_length')));
         $element = new Zend_Form_Element_Password('password');
         $element
             ->setLabel('Password')
             ->setRequired()
-            ->setAttrib('renderPassword', true)
             ->addValidator('StringLength', false, array(
                     $min_password_length, $max_password_length))
+            ->setAttrib('renderPassword', true)
             ;
         $this->addElement($element);
         
+        // Submit
         $element = new Zend_Form_Element_Submit('submit');
         $element
             ->setLabel('Login')
             ->setIgnore(true)
             ;
         $this->addElement($element);
-        $this->loadDefaultDecorators();
-        $this->addDecorator('Errors');
         
-        $this->getDecorator('Errors')->setOption('placement', Zend_Form_Decorator_Abstract::PREPEND);      
+        // Decorators
+        $this->addPrefixPath('FinalView_Form_Decorator', 'FinalView/Form/Decorator', Zend_Form::DECORATOR);
+        $this->loadDefaultDecorators();
+        $this->addDecorator('FvformErrors');
+        $this->getDecorator('FvformErrors')->setOption('placement', Zend_Form_Decorator_Abstract::PREPEND);      
     }
     
 }
