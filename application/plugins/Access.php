b76a72f1d3a1c07560c5909118e2d65559147a60
goldie <yuryz@finalview.com>
2010-07-17 18:53:26 +0300

    access handlers added

--- application/plugins/Access.php
+++ application/plugins/Access.php
@@ -11,4 +11,26 @@ class Application_Plugin_Access extends FinalView_Controller_Plugin_Access
     {
         Zend_Controller_Action_HelperBroker::getStaticHelper('Redirector')->gotoRoute(array(), 'AdminAuthLogin' );
     }
+        
+    protected function _notFoundIfAdminAuthorized()
+    {
+        $failedRule = $this->getResource()->getAccessRule();
+        
+        if ($failedRule->isFailedRule('admin_logged_in')) {
+        	$this->_redirectToLogin();
+        }
+        
+        $this->_notFoundHandler();
+    }
+
+    protected function _forbiddenIfAdminAuthorized()
+    {
+        $failedRule = $this->getResource()->getAccessRule();
+        
+        if ($failedRule->isFailedRule('admin_logged_in')) {
+        	$this->_redirectToLogin();
+        }
+        
+        $this->_forbiddenHandler();
+    }               
 }
