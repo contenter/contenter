6643d31069926af9a07a9e96e77deeae3667c1cb
goldie <yuryz@finalview.com>
2010-10-14 18:03:53 +0300

    setRoute method overwrited
    Signed-off-by: goldie <yuryz@finalview.com>

--- library/FinalView/Navigation/Page/Cms.php
+++ library/FinalView/Navigation/Page/Cms.php
@@ -9,15 +9,22 @@ class FinalView_Navigation_Page_Cms extends FinalView_Navigation_Page
         $this->setRoute($name);
         $this->setParams(array('page_name' => $name));
     }
-    
+
     public function getPageName()
     {
         return $this->_name;
     }
-    
+
     public function setRoute($route)
     {
-        parent::setRoute($route);
+        if (null !== $route && (!is_string($route) || strlen($route) < 1)) {
+            require_once 'Zend/Navigation/Exception.php';
+            throw new Zend_Navigation_Exception(
+                 'Invalid argument: $route must be a non-empty string or null');
+        }
+
+        $this->_route = $route;
+        $this->_hrefCache = null;
 
         $this->setModule('Cms');
         $this->setController('index');
