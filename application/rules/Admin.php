50df3c1fb23bba87c3262ff5085850700c60a0a0
Yury Zolotarsky <yuryz@finalview.com>
2011-01-13 23:26:06 +0200

    resources changed
    Signed-off-by: Yury Zolotarsky <yuryz@finalview.com>

--- application/rules/Admin.php
+++ application/rules/Admin.php
@@ -4,12 +4,16 @@ class Application_Rules_Admin extends FinalView_Access_Rules_Abstract
     
     public function adminLoggedInRule()
     {        
+        if (!FinalView_Auth::getInstance()->hasIdentity()) {
+            return false;
+        }
+
         $admin = FinalView_Auth::getInstance()->getAuthEntity(array(
             'role'  =>  Roles::USER_BACKEND
         ));
-        
+
         if ($admin) {
-        	return true;
+            return true;
         }
         
         return false;
