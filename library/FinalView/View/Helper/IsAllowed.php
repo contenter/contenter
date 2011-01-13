4740bd41a83b8d585712697bfbf459e49895f505
Yury Zolotarsky <yuryz@finalview.com>
2011-01-14 01:39:36 +0200

    isAllowed helpers bugfixing

--- library/FinalView/View/Helper/IsAllowed.php
+++ library/FinalView/View/Helper/IsAllowed.php
@@ -3,7 +3,7 @@
 class FinalView_View_Helper_IsAllowed extends Zend_View_Helper_Abstract
 {
 
-    public function isAllowed($resource, $params = array(), $mode = FinalView_Application_Resource::ACCESS_MODE_SOFT)
+    public function isAllowed($resource, $params = array(), $mode = FinalView_Application_Resources::ACCESS_MODE_SOFT)
     {
         return
             Zend_Controller_Action_HelperBroker::getStaticHelper('IsAllowed')
