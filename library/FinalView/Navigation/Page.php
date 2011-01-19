e07eb1c4a2351911dc8fe36ea7baa8688db5737e
Yury Zolotarsky <yuryz@finalview.com>
2011-01-19 15:29:14 +0200

    new resources separator applied to FinalView_Navigation_Page
    Signed-off-by: Yury Zolotarsky <yuryz@finalview.com>

--- library/FinalView/Navigation/Page.php
+++ library/FinalView/Navigation/Page.php
@@ -1,27 +1,27 @@
 <?php
 class FinalView_Navigation_Page extends Zend_Navigation_Page_Mvc
-{   
-    
+{
+
     protected $_params = null;
     protected $_activeForRoutes = array();
-    
+
     public function isActive($recursive = false)
     {
         if (!$this->_active) {
             $front = Zend_Controller_Front::getInstance();
-            
+
             $routerName = $front->getRouter()->getCurrentRouteName();
-            
+
             if ($this->isActiveForRoute($routerName) ){
                 return true;
             }
-            
+
             if ($routerName != $this->getRoute()) {
                 return parent::isActive($recursive);
             }
-            
+
             $params = array_intersect_assoc($this->getParams(), $front->getRequest()->getParams());
-            
+
             if (count($params) == count($this->getParams()) ) {
                 $this->_active = true;
                 return true;
@@ -30,39 +30,39 @@ class FinalView_Navigation_Page extends Zend_Navigation_Page_Mvc
 
         return parent::isActive($recursive);
     }
-    
+
     protected function _generateResource()
     {
-    	$router = Zend_Controller_Front::getInstance()->getRouter();
-    	$route = $router->getRoute($this->getRoute() );
-    	$resource = $route->getDefault('module') . '-' . $route->getDefault('controller') . '-' . $route->getDefault('action');
-        
-        return new FinalView_Acl_Resource($resource, $this->getParams());        
+        $router = Zend_Controller_Front::getInstance()->getRouter();
+        $route = $router->getRoute($this->getRoute() );
+        $resource = $route->getDefault('module') . '.' . $route->getDefault('controller') . '.' . $route->getDefault('action');
+
+        return new FinalView_Acl_Resource($resource, $this->getParams());
     }
-    
+
     public function attachAclResource()
     {
         $this->setResource($this->_generateResource());
     }
-    
+
     public function getResource()
     {
         if (is_null($this->_resource)) {
-        	$this->attachAclResource();        	
+            $this->attachAclResource();
         }
-        
+
         return parent::getResource();
     }
-    
+
     public function setResource($resource = null)
     {
         if (is_string($resource)) {
-        	return parent::setResource(new FinalView_Acl_Resource($resource, $this->getParams()));
+            return parent::setResource(new FinalView_Acl_Resource($resource, $this->getParams()));
         }
-        
+
         return parent::setResource($resource);
     }
-    
+
     public function getParams()
     {
         if (null === $this->_params) {
@@ -74,27 +74,27 @@ class FinalView_Navigation_Page extends Zend_Navigation_Page_Mvc
         }
         return (array)parent::getParams();
     }
-    
+
     public function setActiveForRoutes($routes)
     {
         $this->_activeForRoutes = $routes;
     }
-    
+
     public function isActiveForRoute($route)
     {
         return in_array($route, $this->_activeForRoutes);
     }
-    
+
     public function setRoute($route)
     {
         parent::setRoute($route);
-        
+
         $pageRoute = Zend_Controller_Front::getInstance()->getRouter()->getRoute($this->_route);
 
         $this->setModule($pageRoute->getDefault('module'));
         $this->setController($pageRoute->getDefault('controller'));
         $this->setAction($pageRoute->getDefault('action'));
-        
+
         return $this;
     }
 }
